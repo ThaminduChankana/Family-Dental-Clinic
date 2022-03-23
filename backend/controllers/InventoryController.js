40cee744877e226f2dfa82f91b8325c924164416
BawanthiGunasekara <bawanthik99@gmail.com>
2022-03-23 12:40:24 +0530

    implemented the backend for the inventory

---
+++ backend/controllers/InventoryController.js
@@ -0,0 +1,73 @@
+const Inventory = require("../models/InventoryModels");
+const asyncHandler = require("express-async-handler");
+
+const getInventory = asyncHandler(async(req, res) => {
+    const inventory = await Inventory.find();
+    res.json(inventory);
+});
+
+const CreateInventory = asyncHandler(async(req, res) => {
+    const { productName, quantity, description } = req.body;
+
+    if (!productName || !quantity || !description) {
+        res.status(400);
+        throw new Error("Please input details to all the feilds");
+    } else {
+        const inventory = new Inventory({
+            productName,
+            quantity,
+            description,
+        });
+
+        const createdInventory = await inventory.save();
+        res.status(201).json(createdInventory);
+    }
+});
+
+const getInventoryById = asyncHandler(async(req, res) => {
+    const inventory = await Inventory.findById(req.params.id);
+
+    if (inventory) {
+        res.json(inventory);
+    } else {
+        res.status(404).json({ message: "Inventory not found" });
+    }
+});
+
+const UpdateInventory = asyncHandler(async(req, res) => {
+    const { productName, quantity, description } = req.body;
+
+    const inventory = await Inventory.findById(req.params.id);
+
+    if (inventory) {
+        inventory.productName = productName;
+        inventory.quantity = quantity;
+        inventory.description = description;
+
+        const updatedinventory = await inventory.save();
+        res.json(updatedinventory);
+    } else {
+        res.status(404);
+        throw new Error("Inventory not found");
+    }
+});
+
+const DeleteInventory = asyncHandler(async(req, res) => {
+    const inventory = await Inventory.findById(req.params.id);
+
+    if (inventory) {
+        await inventory.remove();
+        res.json({ message: "Removed the Inventory" });
+    } else {
+        res.status(404);
+        throw new Error("Inventory not found");
+    }
+});
+
+module.exports = {
+    getInventory,
+    CreateInventory,
+    getInventoryById,
+    UpdateInventory,
+    DeleteInventory,
+};
--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,22 @@ const adminRoutes = require("./routes/adminRoutes");
 const doctorRoutes = require("./routes/doctorRoutes");
 const patientRoutes = require("./routes/patientRoutes");
 const connectDB = require("./config/db");
+const inventoryRoutes = require("./routes/InventoryRoutes");
 
 dotenv.config();
 connectDB();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("API is Running");
+    res.send("API is Running");
 });
 
 app.use("/user/admin", adminRoutes);
 app.use("/user/doctor", doctorRoutes);
 app.use("/user/patient", patientRoutes);
+
+//Importing the Inventory Route
+app.use("/user/inventory", inventoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server Started on port ${PORT}..`));
