2da396ea6d479bd8a12fd67fce62dd2eae825a34
Chathurika <Chathurika@LAPTOP-DGFGMJLI>
2022-03-24 08:34:42 +0530

    Add treatment routes to the doctor routes

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -7,6 +7,27 @@ const {
 	deleteDoctorProfile,
 } = require("../controllers/doctorController");
 const { getPatients, getPatientProfileById } = require("../controllers/patientController");
+const {
+	getBasicTreatments,
+	CreateBasicTreatment,
+	getBasicTreatmentById,
+	UpdateBasicTreatment,
+	DeleteBasicTreatment,
+} = require("../controllers/basicTreatmentController");
+const {
+	getOrthodontics,
+	CreateOrthodontic,
+	getOrthodonticById,
+	UpdateOrthodontic,
+	DeleteOrthodontic,
+} = require("../controllers/orthodonticController");
+const {
+	getFillings,
+	CreateFilling,
+	getFillingtById,
+	UpdateFilling,
+	DeleteFilling,
+} = require("../controllers/fillingController");
 const { protect } = require("../middlewares/authDoctorMiddleware");
 const router = express.Router();
 
@@ -16,6 +37,28 @@ router.route("/login").post(authDoctor);
 router.route("/edit").put(protect, updateDoctorProfile);
 router.route("/view").get(protect, getDoctorProfile);
 router.route("/delete").delete(protect, deleteDoctorProfile);
+
+router.route("/treatment/basic-treatment/get").get(protect, getBasicTreatments);
+router.route("/treatment/basic-treatment/create").post(protect, CreateBasicTreatment);
+router
+	.route("/treatment/basic-treatment/get/:id")
+	.get(protect, getBasicTreatmentById)
+	.put(protect, UpdateBasicTreatment)
+	.delete(protect, DeleteBasicTreatment);
+router.route("/treatment/orthodontic/get").get(protect, getOrthodontics);
+router.route("/treatment/orthodontic/create").post(protect, CreateOrthodontic);
+router
+	.route("/treatment/orthodontic/get/:id")
+	.get(protect, getOrthodonticById)
+	.put(protect, UpdateOrthodontic)
+	.delete(protect, DeleteOrthodontic);
+router.route("/treatment/filling/get").get(protect, getFillings);
+router.route("/treatment/filling/create").post(protect, CreateFilling);
+router
+	.route("/treatment/filling/get/:id")
+	.get(protect, getFillingtById)
+	.put(protect, UpdateFilling)
+	.delete(protect, DeleteFilling);
 
 //Routes for Patient account operations
 router.route("/patients").get(protect, getPatients);
