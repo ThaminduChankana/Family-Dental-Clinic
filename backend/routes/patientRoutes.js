58d6f840a750e39b91062849198cdde6683377ee
kawyasamarasinghe <it20604608@my.sliit.lk>
2022-04-19 18:07:10 +0530

    Implement backend for feedback and Q&A

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -6,6 +6,23 @@ const {
 	updatePatientProfile,
 	deletePatientProfile,
 } = require("../controllers/patientController");
+
+const {
+	createFeedback,
+	updateFeedback,
+	getFeedback,
+	DeleteFeedback,
+	getFeedbackById,
+} = require("../controllers/feedbackController");
+
+const {
+	createQuestion,
+	UpdateQuestion,
+	getQuestion,
+	getQuestionById,
+	DeleteQuestion,
+} = require("../controllers/questionController");
+
 const { getMedicalHistoryById } = require("../controllers/medicalHistoryController");
 const { protect } = require("../middlewares/authPatientMiddleware");
 const router = express.Router();
@@ -19,5 +36,19 @@ router.route("/delete").delete(protect, deletePatientProfile);
 
 //Routes for patient to get medical history
 router.route("/medical_history/:id").get(protect, getMedicalHistoryById);
+
+//Routes for feedback operations
+router.route("/feedback/create").post(protect, createFeedback);
+router.route("/feedback/update/:id").put(protect, updateFeedback);
+router.route("/feedback/view/:id").get(protect, getFeedbackById);
+router.route("/feedback/view").get(protect, getFeedback);
+router.route("/feedback/delete/:id").delete(protect, DeleteFeedback);
+
+//Routes for Q&A operations
+router.route("/question/create").post(protect, createQuestion);
+router.route("/question/update/:id").put(protect, UpdateQuestion);
+router.route("/question/view").get(protect, getQuestion);
+router.route("/question/view/:id").get(protect, getQuestionById);
+router.route("/question/delete/:id").delete(protect, DeleteQuestion);
 
 module.exports = router;
