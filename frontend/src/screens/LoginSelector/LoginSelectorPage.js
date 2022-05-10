7641a3789bd90e3f4f29beed86b49558e33acc6b
ThaminduChankana <it20604530@my.sliit.lk>
2022-05-10 23:27:53 +0530

    added css to login selector

--- frontend/src/screens/LoginSelector/LoginSelectorPage.js
+++ frontend/src/screens/LoginSelector/LoginSelectorPage.js
@@ -1,30 +1,29 @@
 import { useEffect } from "react";
-import { Container, Row, Button,Card } from "react-bootstrap";
+import { Container, Row, Button, Card } from "react-bootstrap";
 import MainScreen from "../../components/MainScreen";
 import "./LoginSelector.css";
 
 const LoginSelectorPage = () => {
 	return (
 		<div className="loginSelectBg">
-		<MainScreen title={'Log in Here ...'}>
-				<br></br><br></br>
-			<Card style={{
+			<MainScreen title={"Log in Here ..."}>
+				<br></br>
+				<br></br>
+				<Card
+					style={{
 						borderRadius: 45,
 						borderWidth: 2.0,
 						marginTop: 80,
 						paddingInline: 10,
 						background: "rgba(231, 238, 238, 0.8)",
-					}}><div className="loginSelect">
-				
-				
-					<div className="intro-text" style={{marginTop:10}} >
-				
-					<br></br><br></br>
-				
-					
-					
+					}}
+				>
+					<div className="loginSelect">
+						<div className="intro-text" style={{ marginTop: 10 }}>
+							<br></br>
+							<br></br>
+
 							<a href="/admin-login">
-								
 								<Button size="lg" className="landingbutton">
 									Admin Login
 								</Button>
@@ -42,16 +41,15 @@ const LoginSelectorPage = () => {
 								</Button>
 							</a>
 
-							
-					<br></br><br></br>
-					<br></br><br></br>
+							<br></br>
+							<br></br>
+							<br></br>
+							<br></br>
 						</div>
 					</div>
-			
-		
-			</Card>
+				</Card>
 			</MainScreen>
-			</div>
+		</div>
 	);
 };
 
