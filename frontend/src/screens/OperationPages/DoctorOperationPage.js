d6795a407c16f03880c1d88e46c83653c46edf63
ThaminduChankana <it20604530@my.sliit.lk>
2022-05-19 19:59:36 +0530

    added security to user accounts

--- frontend/src/screens/OperationPages/DoctorOperationPage.js
+++ frontend/src/screens/OperationPages/DoctorOperationPage.js
@@ -12,87 +12,95 @@ const DoctorOperationPage = ({ history }) => {
 		dispatch(doctorLogout());
 		history.push("/");
 	};
-
-	return (
-		<div className="doctorBackground">
-			<MainScreen title={`Welcome Back ${doctorInfo && doctorInfo.name} ...`}>
-				<Button variant="danger" onClick={logoutHandler} className="logoutBtn" style={{ float: "right" }}>
-					Logout
-				</Button>
+	if (doctorInfo) {
+		return (
+			<div className="doctorBackground">
+				<MainScreen title={`Welcome Back ${doctorInfo && doctorInfo.name} ...`}>
+					<Button variant="danger" onClick={logoutHandler} className="logoutBtn" style={{ float: "right" }}>
+						Logout
+					</Button>
+					<br></br>
+					<div className="loginContainer">
+						<Card
+							style={{
+								borderRadius: 45,
+								borderWidth: 2.0,
+								marginTop: 20,
+								paddingInline: 10,
+								background: "rgba(231, 238, 238, 0.8)",
+								marginLeft: "10%",
+								marginRight: "10%",
+							}}
+						>
+							<div className="intro-text">
+								<br></br>
+								<br></br>
+								<div>
+									<a href="/doctor-view">
+										<Button size="lg" style={{ width: 350, height: 75 }}>
+											My Account
+										</Button>
+									</a>
+									&emsp;
+									<a href="/">
+										<Button size="lg" style={{ width: 350, height: 75 }}>
+											Doctor's Schedule Management
+										</Button>
+									</a>
+								</div>
+								<br></br>
+								<div>
+									<a href="/doctor-patients">
+										<Button size="lg" style={{ width: 350, height: 75 }}>
+											Patient Management
+										</Button>
+									</a>
+								</div>
+								<div className="buttonContainer">
+									<a href="/treatment-orthodontic-view">
+										<Button size="lg" className="landingbutton">
+											Patient Diagnosis Management
+										</Button>
+									</a>
+								</div>
+								<br></br>
+								<div>
+									<a href="/">
+										<Button size="lg" style={{ width: 350, height: 75 }}>
+											Appointment Management
+										</Button>
+									</a>
+									&emsp;
+									<a href="/">
+										<Button size="lg" style={{ width: 350, height: 75 }}>
+											Patient Medical History Management
+										</Button>
+									</a>
+								</div>
+								<br></br>
+								<div>
+									<a href="/">
+										<Button size="lg" style={{ width: 350, height: 75 }}>
+											Doctor's Blog Management
+										</Button>
+									</a>
+								</div>
+							</div>
+							<br></br>
+							<br></br>
+						</Card>
+					</div>
+				</MainScreen>
+			</div>
+		);
+	} else {
+		return (
+			<div className="denied">
+				<MainScreen />
 				<br></br>
-				<div className="loginContainer">
-					<Card
-						style={{
-							borderRadius: 45,
-							borderWidth: 2.0,
-							marginTop: 20,
-							paddingInline: 10,
-							background: "rgba(231, 238, 238, 0.8)",
-							marginLeft: "10%",
-							marginRight: "10%",
-						}}
-					>
-						<div className="intro-text">
-							<br></br>
-							<br></br>
-							<div>
-								<a href="/doctor-view">
-									<Button size="lg" style={{ width: 350, height: 75 }}>
-										My Account
-									</Button>
-								</a>
-								&emsp;
-								<a href="/">
-									<Button size="lg" style={{ width: 350, height: 75 }}>
-										Doctor's Schedule Management
-									</Button>
-								</a>
-							</div>
-							<br></br>
-							<div>
-								<a href="/doctor-patients">
-									<Button size="lg" style={{ width: 350, height: 75 }}>
-										Patient Management
-									</Button>
-								</a>
-							</div>
-							<div className="buttonContainer">
-								<a href="/treatment-orthodontic-view">
-									<Button size="lg" className="landingbutton">
-										Patient Diagnosis Management
-									</Button>
-								</a>
-							</div>
-							<br></br>
-							<div>
-								<a href="/">
-									<Button size="lg" style={{ width: 350, height: 75 }}>
-										Appointment Management
-									</Button>
-								</a>
-								&emsp;
-								<a href="/">
-									<Button size="lg" style={{ width: 350, height: 75 }}>
-										Patient Medical History Management
-									</Button>
-								</a>
-							</div>
-							<br></br>
-							<div>
-								<a href="/">
-									<Button size="lg" style={{ width: 350, height: 75 }}>
-										Doctor's Blog Management
-									</Button>
-								</a>
-							</div>
-						</div>
-						<br></br>
-						<br></br>
-					</Card>
-				</div>
-			</MainScreen>
-		</div>
-	);
+			</div>
+		);
+	}
 };
 
 export default DoctorOperationPage;
--- frontend/src/screens/UserManagement/EditScreens/AdminEditScreen.js
+++ frontend/src/screens/UserManagement/EditScreens/AdminEditScreen.js
@@ -82,185 +82,193 @@ const AdminEditScreen = () => {
 			dispatch(adminUpdateProfile(adminUpdatedInfo));
 		}
 	};
-
-	return (
-		<div className="editBg">
-			<MainScreen title="EDIT PROFILE - ADMIN">
-				<Button
-					style={{
-						float: "left",
-						marginTop: 5,
-						fontSize: 15,
-					}}
-					href="/admin"
-				>
-					{" "}
-					Back to Operations Page
-				</Button>
-				<br></br>
-				<br></br>
-				<Card
-					style={{
-						borderRadius: 45,
-						borderWidth: 2.0,
-						marginTop: 20,
-						paddingInline: 10,
-						background: "rgba(231, 238, 238, 0.8)",
-					}}
-				>
-					<div className="loginContainer">
-						<div>
-							{error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-							{message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
-							{loading && <Loading />}
-						</div>
-						<Row className="AdminProfileContainer">
-							<Col md={6}>
-								<Form onSubmit={submitHandler}>
-									<Form.Group controlId="adminName">
-										<Form.Label>Name</Form.Label>
-										<Form.Control
-											type="name"
-											value={name}
-											placeholder="Enter name"
-											onChange={(e) => setName(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="adminDob">
-										<Form.Label>Date Of Birth</Form.Label>
-										<Form.Control type="date" value={dob} onChange={(e) => setDob(e.target.value)} required />
-									</Form.Group>
-									<Form.Group controlId="adminFormBasicNic">
-										<Form.Label>NIC Number</Form.Label>
-										<Form.Control
-											type="text"
-											value={nic}
-											placeholder="Enter NIC"
-											onChange={(e) => setNic(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="adminFormBasicTelephone">
-										<Form.Label>Telephone</Form.Label>
-										<Form.Control
-											type="text"
-											value={telephone}
-											placeholder="Enter Telephone Number"
-											onChange={(e) => setTelephone(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="adminFormBasicAddress">
-										<Form.Label>Address</Form.Label>
-										<Form.Control
-											type="textArea"
-											value={address}
-											placeholder="Enter Address"
-											onChange={(e) => setAddress(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="adminFormBasicEmail">
-										<Form.Label>Email</Form.Label>
-										<Form.Control
-											type="email"
-											value={email}
-											placeholder="Enter Email Address"
-											onChange={(e) => setEmail(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="adminFormBasicPreviousRef">
-										<Form.Label>Previous References</Form.Label>
-										<Form.Control
-											type="textArea"
-											value={previousRef}
-											placeholder="Enter Previous References"
-											onChange={(e) => setPreviousRef(e.target.value)}
-											required
-										/>
-									</Form.Group>
-									<Form.Group controlId="formBasicPassword">
-										<Form.Label>Password</Form.Label>
-										<Form.Control
-											type="password"
-											value={password}
-											placeholder="Password"
-											onChange={(e) => setPassword(e.target.value)}
-										/>
-									</Form.Group>
-									<Form.Group controlId="confirmPassword">
-										<Form.Label>Confirm Password</Form.Label>
-										<Form.Control
-											type="password"
-											value={confirmpassword}
-											placeholder="Confirm Password"
-											onChange={(e) => setConfirmPassword(e.target.value)}
-										/>
-									</Form.Group>
-									{picMessage && <ErrorMessage variant="danger">{picMessage}</ErrorMessage>}
-									<Form.Group controlId="pic">
-										<Form.Label>Profile Picture</Form.Label>
-										<Form.File
-											onChange={(e) => postDetails(e.target.files[0])}
-											id="custom-file"
-											type="image/png"
-											label="Upload Profile Picture"
-											custom
-										/>
-									</Form.Group>
-									<br></br>
-									<Button
-										variant="danger"
-										type="submit"
-										style={{
-											fontSize: 15,
-										}}
-									>
-										Update
-									</Button>
+	if (adminInfo) {
+		return (
+			<div className="editBg">
+				<MainScreen title="EDIT PROFILE - ADMIN">
+					<Button
+						style={{
+							float: "left",
+							marginTop: 5,
+							fontSize: 15,
+						}}
+						href="/admin"
+					>
+						{" "}
+						Back to Operations Page
+					</Button>
+					<br></br>
+					<br></br>
+					<Card
+						style={{
+							borderRadius: 45,
+							borderWidth: 2.0,
+							marginTop: 20,
+							paddingInline: 10,
+							background: "rgba(231, 238, 238, 0.8)",
+						}}
+					>
+						<div className="loginContainer">
+							<div>
+								{error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+								{message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+								{loading && <Loading />}
+							</div>
+							<Row className="AdminProfileContainer">
+								<Col md={6}>
+									<Form onSubmit={submitHandler}>
+										<Form.Group controlId="adminName">
+											<Form.Label>Name</Form.Label>
+											<Form.Control
+												type="name"
+												value={name}
+												placeholder="Enter name"
+												onChange={(e) => setName(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="adminDob">
+											<Form.Label>Date Of Birth</Form.Label>
+											<Form.Control type="date" value={dob} onChange={(e) => setDob(e.target.value)} required />
+										</Form.Group>
+										<Form.Group controlId="adminFormBasicNic">
+											<Form.Label>NIC Number</Form.Label>
+											<Form.Control
+												type="text"
+												value={nic}
+												placeholder="Enter NIC"
+												onChange={(e) => setNic(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="adminFormBasicTelephone">
+											<Form.Label>Telephone</Form.Label>
+											<Form.Control
+												type="text"
+												value={telephone}
+												placeholder="Enter Telephone Number"
+												onChange={(e) => setTelephone(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="adminFormBasicAddress">
+											<Form.Label>Address</Form.Label>
+											<Form.Control
+												type="textArea"
+												value={address}
+												placeholder="Enter Address"
+												onChange={(e) => setAddress(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="adminFormBasicEmail">
+											<Form.Label>Email</Form.Label>
+											<Form.Control
+												type="email"
+												value={email}
+												placeholder="Enter Email Address"
+												onChange={(e) => setEmail(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="adminFormBasicPreviousRef">
+											<Form.Label>Previous References</Form.Label>
+											<Form.Control
+												type="textArea"
+												value={previousRef}
+												placeholder="Enter Previous References"
+												onChange={(e) => setPreviousRef(e.target.value)}
+												required
+											/>
+										</Form.Group>
+										<Form.Group controlId="formBasicPassword">
+											<Form.Label>Password</Form.Label>
+											<Form.Control
+												type="password"
+												value={password}
+												placeholder="Password"
+												onChange={(e) => setPassword(e.target.value)}
+											/>
+										</Form.Group>
+										<Form.Group controlId="confirmPassword">
+											<Form.Label>Confirm Password</Form.Label>
+											<Form.Control
+												type="password"
+												value={confirmpassword}
+												placeholder="Confirm Password"
+												onChange={(e) => setConfirmPassword(e.target.value)}
+											/>
+										</Form.Group>
+										{picMessage && <ErrorMessage variant="danger">{picMessage}</ErrorMessage>}
+										<Form.Group controlId="pic">
+											<Form.Label>Profile Picture</Form.Label>
+											<Form.File
+												onChange={(e) => postDetails(e.target.files[0])}
+												id="custom-file"
+												type="image/png"
+												label="Upload Profile Picture"
+												custom
+											/>
+										</Form.Group>
+										<br></br>
+										<Button
+											variant="danger"
+											type="submit"
+											style={{
+												fontSize: 15,
+											}}
+										>
+											Update
+										</Button>
+										&emsp;
+										<Button
+											variant="primary"
+											href="/admin-view"
+											style={{
+												fontSize: 15,
+											}}
+										>
+											View Profile
+										</Button>
+									</Form>
 									&emsp;
-									<Button
-										variant="primary"
-										href="/admin-view"
-										style={{
-											fontSize: 15,
-										}}
-									>
-										View Profile
-									</Button>
-								</Form>
-								&emsp;
-							</Col>
-							<Col
-								style={{
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-								}}
-							>
-								<img
-									src={pic}
-									alt={name}
-									className="profilePic"
+								</Col>
+								<Col
 									style={{
-										boxShadow: "7px 7px 20px ",
-										borderColor: "black",
-										borderRadius: 250,
-										background: "white",
-										width: "50%",
-										height: "30%",
+										display: "flex",
+										alignItems: "center",
+										justifyContent: "center",
 									}}
-								/>
-							</Col>
-						</Row>
-					</div>
-				</Card>
+								>
+									<img
+										src={pic}
+										alt={name}
+										className="profilePic"
+										style={{
+											boxShadow: "7px 7px 20px ",
+											borderColor: "black",
+											borderRadius: 250,
+											background: "white",
+											width: "50%",
+											height: "30%",
+										}}
+									/>
+								</Col>
+							</Row>
+						</div>
+					</Card>
+					<br></br>
+				</MainScreen>
+			</div>
+		);
+	} else {
+		return (
+			<div className="denied">
+				<MainScreen />
 				<br></br>
-			</MainScreen>
-		</div>
-	);
+			</div>
+		);
+	}
 };
 
 export default AdminEditScreen;
